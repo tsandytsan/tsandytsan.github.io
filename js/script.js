8a11ff40a1259e94fcdcf0cafa4e51309296ac4a
tsandytsan <tsandytsan@gmail.com>
2018-07-09 01:44:02 -0700

    working draft

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-// Mobile/Small screen navigation
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
-// scrollSpy function for navigation
-$(document).ready(function(){
-  $('.scrollspy').scrollSpy();
-});
+// var numOfClicks = 0;
+//
+// function showToast(){
+//   numOfClicks = numOfClicks + 1;
+//   if (numOfClicks == 1){
+//     M.toast({'One cookie for you!'});
+//   }
+//   else {
+//     M.toast({ numOfClicks + ' cookies for you!'});
+//   }
+// }
