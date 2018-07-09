50e3fc594b6285b105deaa6255a8fa77c3bc09ce
tsandytsan <tsandytsan@gmail.com>
2018-07-08 20:25:36 -0700

    working draft of site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Mobile/Small screen navigation
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+// scrollSpy function for navigation
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
