173b4c4da6cae83b68cff357609cc60acd08d69f
tsandytsan <tsandytsan@gmail.com>
2021-09-18 13:46:56 -0700

    added new projects page

---
+++ js/designs_script.js
@@ -0,0 +1,32 @@
+// let mode_toggle = document.querySelector("#ui")
+//
+// mode_toggle.addEventListener("click",()=>{
+//   if (document.body.classList.contains("dark")){
+//     lightMode()
+//   } else {
+//     darkMode()
+//   }
+// })
+//
+// function darkMode(){
+//   document.body.classList.add("dark")
+//   var sunmoon = document.querySelector("#ui")
+//   sunmoon.setAttribute("src","sun.svg")
+// }
+//
+// function lightMode(){
+//   document.body.classList.remove("dark")
+//   var sunmoon = document.querySelector("#ui")
+//   sunmoon.setAttribute("src","moon.svg")
+// }
+//
+
+// enable the parallax
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+// enable the mobile side navigation
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
