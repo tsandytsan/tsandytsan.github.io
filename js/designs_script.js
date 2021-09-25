b5057bca2ea9288e40d81a5794affdac4f492da5
tsandytsan <tsandytsan@gmail.com>
2021-09-24 23:01:58 -0700

    transferring changes onto laptop

--- js/designs_script.js
+++ js/designs_script.js
@@ -30,3 +30,15 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
+
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.materialboxed');
+//   var instances = M.Materialbox.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
